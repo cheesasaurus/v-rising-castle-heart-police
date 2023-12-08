fb9a710b705528da6964a0da21b45ac66863648b
Nicholas Toby <cheesasaurus@gmail.com>
2023-12-08 08:04:21 -0600

    MapPainter: show territory scores

--- MapPainter/main.js
+++ MapPainter/main.js
@@ -5,6 +5,9 @@ const backgroundImage = new Image();
 await new Promise(resolve => backgroundImage.onload = resolve, backgroundImage.src = "./images/map-background.png");
 
 
+const showBoundingRectangle = true;
+const showTerritoryId = true;
+
 const scaleDown = 2.13;
 const offsetX = -452;
 const offsetY = 166;
@@ -28,20 +31,42 @@ for (const territory of data.Territories) {
     const x = (min.x / scaleDown) + offsetX;
     const y = ctx.canvas.height - (max.y / scaleDown) + offsetY;
 
-    ctx.strokeStyle = "magenta"
-    ctx.beginPath();
-    ctx.rect(x, y, width, height);
-    ctx.stroke();
+    // bounding rectangle
+    if (showBoundingRectangle) {
+        ctx.strokeStyle = "magenta";
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.rect(x, y, width, height);
+        ctx.stroke();
+    }
 
     const centerX = x + (width / 2);
     const centerY = y + (height / 2);
-    const text = territory.CastleTerritoryId;
 
-    ctx.font = "20px Arial";
+    // score
+    const scoreText = territory.Score;
+    const scoreY = centerY - 5;
+    ctx.font = "bold 26px Roboto";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
-    ctx.fillStyle = "white"
-    ctx.strokeStyle = "black"
-    ctx.strokeText(text, centerX, centerY);
-    ctx.fillText(text, centerX, centerY);
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.strokeText(scoreText, centerX, scoreY);
+    ctx.fillText(scoreText, centerX, scoreY);
+
+    // territory id
+    if (showTerritoryId) {
+        const idText = '#' + territory.CastleTerritoryId;
+        const idY = centerY + 15;
+        ctx.font = "12px Arial";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "white";
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 1;
+        ctx.strokeText(idText, centerX, idY);
+        ctx.fillText(idText, centerX, idY);
+    }
+    
 }
